f5bce86dec7f4a26b6dc60f7c982b2346f3c0f59
Serhan ÇINAR <serhancinar@post.com>
2026-02-04 01:52:46 -0500

    Post GET route added

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hELLO World!");
+  res.send("Hello World!");
+});
+
+app.get("/posts", (req, res) => {
+  res.send("Post Route...");
 });
 
 app.listen(3000, () => {
